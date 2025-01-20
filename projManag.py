a388b5a9b4ac9409c27d2d9d01209f5328e7506c
Gabriel Muñoz <fgabriel1891@gmail.com>
2025-01-20 09:49:26 -0500

    Create projManag.py

---
+++ projManag.py
@@ -0,0 +1,46 @@
+## Project management structure 
+
+### Phase 01: Initiation 
+  - Business case 
+  - Project charter
+  - Launch materials 
+  - Checklist 
+  - Gating process
+  - Change Requests 
+### Phase 02: Planning 
+  - Project plan
+      - approvals
+      - data governance 
+      - ResponsibleAccountableConsultedInformed: Resposibility Assigment Matrix
+      - SignatoryAcccountableExecutor
+  - Schedule
+      - Schedule 
+      - Roadmap 
+      - Task list/matrix
+  - Budget
+      - Cost estimates
+      - Financial Statements 
+      - Invoices
+      - Incremental costs 
+### Phase 03: Monitoring - Controlling 
+
+    - RAID management 
+    - Cost management 
+    - KPI 
+    - Status reports
+    - Meeting materials
+    - Gating process
+    - Change management 
+    - Governance
+    - Privacy and Security
+    - Benefits monitoring 
+    - Allocations 
+### Phase 04: Closure 
+
+    - Lessons learned
+    - Transition-Operationalization
+    - Project closure report 
+    - Gating process 
+
+### Appendix:
+  - Handover
